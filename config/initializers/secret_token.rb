05d877522a36eb6c390256202f6cff239bbc8c4f
Ed <ed@edforshaw.co.uk>
2013-06-25 13:10:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digitalsocial::Application.config.secret_token = '44361bbf3642e671165bcda87584cecc26b98100c022d0dea5dedd16c6364e83ed2fdb0bfca2b5448f61ee35e3cfccb669c8714fd4e1a460671024a384f61e72'
