de06b2a62d6ccb2cb38df473b29262321885e176
Ed <ed@edforshaw.co.uk>
2013-07-23 16:18:51 +0100

    Remove tripod organisation factory

--- spec/factories/tripod_organisations.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :tripod_organisation do
-    name { Faker::Company.name }
-    lat  { "#{rand(-89..89)}.#{"%08d" % rand(10**8)}" }
-    lng  { "#{rand(-179..179)}.#{"%08d" % rand(10**8)}" }
-  end
-end
