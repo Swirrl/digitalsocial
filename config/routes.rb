613e361f961dd02982898a9949a3060bf2f39712
Ed <ed@edforshaw.co.uk>
2013-07-24 13:24:44 +0100

    Removed outdated models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Digitalsocial::Application.routes.draw do
 
-  devise_for :organisations
-
   devise_for :admins
 
   namespace :admin do
@@ -11,8 +9,6 @@ Digitalsocial::Application.routes.draw do
     resources :admins, only: [:index, :edit, :update]
     root to: 'pages#index'
   end
-
-  resources :organisations, only: [:new, :create]
 
   match ':action' => 'site'
   root to: 'site#index'
