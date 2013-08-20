c4771e752996a241d4d20a8591aeb4ab71c733ae
RicSwirrl <ric@swirrl.com>
2013-08-20 14:41:26 +0100

    start to geocoding and map drop/drag

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,6 +8,10 @@ class Address
   field :locality, 'http://www.w3.org/2006/vcard/ns#locality'
   field :region, 'http://www.w3.org/2006/vcard/ns#region'
   field :country, 'http://www.w3.org/2006/vcard/ns#country'
-  field :postalCode, 'http://www.w3.org/2006/vcard/ns#postalCode'
+  field :postal_code, 'http://www.w3.org/2006/vcard/ns#postalCode'
+
+  def initialize(uri=nil, graph_uri=nil)
+    super(uri || "http://data.digitalsocial.eu/id/address/#{Guid.new}")
+  end
 
 end
