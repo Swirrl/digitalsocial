674ae258832137a849deedb35c82aaa90d1a0101
Ed <ed@edforshaw.co.uk>
2013-08-26 11:38:41 +0100

    Invite users step spec

---
+++ spec/features/wizard/invite_users_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'Invite users wizard step' do
+
+  let(:user) { FactoryGirl.create(:user_with_organisations, organisations_count: 1) }
+  let(:organisation) { user.organisation_resources.first }
+
+  background do
+    login_as user, scope: :user
+    visit organisations_build_invite_users_path
+  end
+
+  scenario 'Skipping the invite users step' do
+    page.should have_link('Skip', href: organisations_build_new_project_path)
+    save_and_open_page
+  end
+
+  scenario 'Fill in team member details' do
+    pending
+  end
+  
+end
