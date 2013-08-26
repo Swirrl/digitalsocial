91e7636e2d31e8138a50f93ab388d7821cc7ebdb
Ed <ed@edforshaw.co.uk>
2013-08-26 11:38:27 +0100

    Edit origanisation step spec

---
+++ spec/features/wizard/edit_organisation_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature 'Edit organisation wizard step' do
+
+  let(:user) { FactoryGirl.create(:user_with_organisations, organisations_count: 1) }
+  let(:organisation) { user.organisation_resources.first }
+
+  background do
+    login_as user, scope: :user
+    visit organisations_build_edit_organisation_path
+  end
+
+  scenario 'Filling in edit organisation step successfully' do
+    fill_in 'Website', with: 'http://test.com'
+    fill_in 'Twitter', with: '@test'
+    choose '0-5'
+    choose 'Business'
+    
+    click_button 'Next step'
+
+    page.current_url.should include("organisations/build/invite_users")
+  end
+
+  scenario 'Filling in edit organisation step successfully with no details' do
+    click_button 'Next step'
+
+    page.current_url.should include("organisations/build/invite_users")
+  end
+
+  scenario 'Skipping the edit organisation step' do
+    page.should have_link('Skip', href: organisations_build_invite_users_path)
+  end
+  
+end
