8d3545324a243322cea312e2ddd3fc9b786625fb
RicSwirrl <ric@swirrl.com>
2013-09-04 17:51:44 +0100

    change vcard fields

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,11 +4,11 @@ class Address
   rdf_type 'http://www.w3.org/2006/vcard/ns#Address'
   graph_uri Digitalsocial::DATA_GRAPH
 
-  field :street_address, 'http://www.w3.org/2006/vcard/ns#streetAddress'
+  field :street_address, 'http://www.w3.org/2006/vcard/ns#street-address'
   field :locality, 'http://www.w3.org/2006/vcard/ns#locality'
   field :region, 'http://www.w3.org/2006/vcard/ns#region'
-  field :country, 'http://www.w3.org/2006/vcard/ns#country'
-  field :postal_code, 'http://www.w3.org/2006/vcard/ns#postalCode'
+  field :country,  'http://www.w3.org/2006/vcard/ns#country-name'
+  field :postal_code, 'http://www.w3.org/2006/vcard/ns#postal-code'
 
   def initialize(uri=nil, graph_uri=nil)
     super(uri || "http://data.digitalsocial.eu/id/address/#{Guid.new}")
