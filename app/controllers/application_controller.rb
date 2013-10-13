610d43c3e356ae0ec77fc44fb28dbaf123cfa9b7
Rick Moynihan <rick.m@swirrl.com>
2013-10-13 16:10:53 +0100

    Fix broken indentation (convert tabs to spaces)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   rescue_from Mongoid::Errors::DocumentNotFound, :with => :handle_resource_not_found
   rescue_from Tripod::Errors::Timeout, :with => :handle_timeout
 
-	protected
+  protected
 
   def show_partners
     @show_partners = true
@@ -60,40 +60,41 @@ class ApplicationController < ActionController::Base
 
   end
 
-	def layout_by_resource
-	  if devise_controller? && resource_name == :admin
-	    "admin"
-	  else
-	    "application"
-	  end
-	end
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "admin"
+    else
+      "application"
+    end
+  end
 
-	def after_sign_in_path_for(resource)
-	  if resource.is_a?(Admin)
-	    admin_root_path
-	  elsif resource.is_a?(User)
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_root_path
+    elsif resource.is_a?(User)
       session[:user_return_to].nil? ? dashboard_path : session[:user_return_to].to_s
-	  end
-	end
+    end
+  end
 
   # given the last bit of the URI, set the org into the sesh
   def set_current_organisation(org_id)
     session[:org_id] = org_id
   end
 
-	def current_organisation
+  def current_organisation
+
     set_current_organisation(params[:org_id]) if params[:org_id]
 
-		@current_organisation ||=
-		  current_user.organisation_resources.find { |o| o.uri == "http://data.digitalsocial.eu/id/organization/#{session[:org_id]}" } ||
-		  current_user.organisation_resources.first
-	end
+    @current_organisation ||=
+      current_user.organisation_resources.find { |o| o.uri == "http://data.digitalsocial.eu/id/organization/#{session[:org_id]}" } ||
+      current_user.organisation_resources.first
+  end
 
-	def current_organisation_membership
+  def current_organisation_membership
     current_user.organisation_memberships.where(organisation_uri: current_organisation.uri.to_s).first
-	end
+  end
 
-	def override_with_other_params(opts={})
+  def override_with_other_params(opts={})
     opts[:scopes].each do |scope|
       next if params[scope].nil?
 
